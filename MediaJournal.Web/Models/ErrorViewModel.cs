f307f9b208a506e47ff3187063e268397f26fbde
Deputation <deputation2@gmail.com>
2025-01-15 20:51:48 +0100

    Initial commit

---
+++ MediaJournal.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MediaJournal.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
